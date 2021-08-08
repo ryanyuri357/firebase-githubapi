4bafdd6f3337b0bca2ed0c221ea79899146b4678
ryanyuri357 <ryan.yuri357@gmail.com>
2021-08-08 14:03:08 -0500

    add firebase config

--- src/App.js
+++ src/App.js
@@ -21,6 +21,10 @@ import PageNotFound from "./pages/PageNotFound";
 import { UserContext } from "./context/UserContext";
 import Footer from "./layout/Footer";
 import Header from "./layout/Header";
+import FirebaseConfig from "./config/FirebaseConfig";
+
+// init firebase
+firebase.initializeApp(FirebaseConfig);
 
 // App
 const App = () => {
---
+++ src/config/FirebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: "AIzaSyBTf8vOLMWJCl80_PPhlxIqve1TDd3OvIU",
+  authDomain: "mygitapp-56a9f.firebaseapp.com",
+  projectId: "mygitapp-56a9f",
+  storageBucket: "mygitapp-56a9f.appspot.com",
+  messagingSenderId: "293722654901",
+  appId: "1:293722654901:web:a7d74243e39ed6d7b256fc",
+  measurementId: "G-SGZV036D3W",
+};

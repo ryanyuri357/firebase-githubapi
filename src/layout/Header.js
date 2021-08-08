663458efa6948226396520ab6b57b5d7003bcec9
ryanyuri357 <ryan.yuri357@gmail.com>
2021-08-08 13:40:08 -0500

    conditional rendering in navbar

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -13,6 +13,12 @@ import { Link } from "react-router-dom";
 import { UserContext } from "../context/UserContext";
 
 const Header = () => {
+  const context = useContext(UserContext);
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
   return (
     <Navbar color="info" light expand="md">
       <NavbarBrand>
@@ -20,18 +26,33 @@ const Header = () => {
           RYP Firestore App
         </Link>
       </NavbarBrand>
-      <NavbarToggler />
-      <Collapse navbar>
+      <NavbarText className="text-white">
+        {context.user?.email ? context.user.email : ""}
+      </NavbarText>
+      <NavbarToggler onClick={toggle} />
+      <Collapse isOpen={isOpen} navbar>
         <Nav className="ms-auto" navbar>
-          <NavItem>
-            <NavLink className="text-white">Signup</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink className="text-white">Signin</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink className="text-white">Logout</NavLink>
-          </NavItem>
+          {context.user ? (
+            <NavItem>
+              {/*<NavLink className="text-white">Logout</NavLink>*/}
+              <NavLink tag={Link} to="/" className="text-white">
+                Logout
+              </NavLink>
+            </NavItem>
+          ) : (
+            <>
+              <NavItem>
+                <NavLink tag={Link} to="/" className="text-white">
+                  Signup
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink tag={Link} to="/" className="text-white">
+                  Signin
+                </NavLink>
+              </NavItem>
+            </>
+          )}
         </Nav>
       </Collapse>
     </Navbar>
